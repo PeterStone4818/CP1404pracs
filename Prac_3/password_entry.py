375d8ee1cc65213609e6c097d615e3b93606a97c
PeterStone4818 <peter.stone@my.jcu.edu.au>
2018-08-09 13:22:56 +1000

    update to use functions

--- Prac_3/password_entry.py
+++ Prac_3/password_entry.py
@@ -1,7 +1,21 @@
 """Peter Stone"""
 
-password = input("password: ")
-while len(password) < 5:
-    password = input("invalid password ")
-char = len(password)
-print ("*" * char)
+
+def main():
+    password = get_password()
+    censoring(password)
+
+
+def censoring(password):
+    for char in password:
+        print("*", end="")
+
+
+def get_password():
+    password = input("password: ")
+    while len(password) < 5:
+        password = input("invalid password ")
+    return password
+
+
+main()
