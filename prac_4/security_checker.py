bd6ae3bd94c7f5097ce65c543bc127ea172214d2
PeterStone4818 <peter.stone@my.jcu.edu.au>
2018-08-13 14:23:58 +1000

    add and complete security checker file

---
+++ prac_4/security_checker.py
@@ -0,0 +1,7 @@
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("please enter your username ")
+if username in usernames:
+    print("access granted")
+else:
+    print("access denied")
+1
